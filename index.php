61c9a9e04a72d3f15b94263643d2dd035b944916
Felipe Ortiz <felipe.ortiz.oyarzun@gmail.com>
2019-03-12 10:47:20 -0300

    Tabla div's

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+?>
+
+<html>
+    <head>
+        <title>eCommerce Sr.F</title>
+        <meta charset="utf-8">
+        <link rel=StyleSheet href="css/style.css" type="text/css" MEDIA=screen>
+    </head>
+    <body>
+        <div class="tabla"> 
+            Titulo de la tabla
+            <div class="row">
+                <div class="cell">
+                    Columna 1
+                </div>
+                <div class="cell">
+                    Columna 2
+                </div>
+            </div>
+            <div class="row">
+                <div class="cell">
+                    Contenido 1
+                </div>
+                <div class="cell">
+                    Contenido 2
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
